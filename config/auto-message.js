473f18421f709317bc5f05f796696e287c2f95c9
rizzalbayu <rizzalbayu21@gmail.com>
2022-08-09 05:26:10 +0000

    bug fix broadcast auto message

--- config/auto-message.js
+++ config/auto-message.js
@@ -1,18 +1,15 @@
 module.exports = {
   // array 0 = key(trigger), array 1 = response, array 2 = include, array 3 = with author, array 4 = reply or broadcast channel
-  reply: (msg, data) => {
-    console.log(data);
-    const message = msg.content.toLowerCase();
-    if (message == data[0] && !data[2])
-      msg.reply(data[1] + (data[3] ? ` ${msg.author.username}` : ''));
-    if (message.includes(data[0]) && data[2])
-      msg.reply(data[1] + (data[3] ? ` ${msg.author.username}` : ''));
+  static: (msg, data) => {
+    if (data[4] == 'reply')
+      msg.reply(`${data[1]} ${data[3] ? msg.author.username : ''}`);
+    if (data[4] == 'channel')
+      msg.channel.send(`${data[1]} ${data[3] ? msg.author.username : ''}`);
   },
-  channel: (msg, data) => {
-    const message = msg.content.toLowerCase();
-    if (message == data[0] && !data[2])
-      msg.reply(data[1] + (data[3] ? ` ${msg.author.username}` : ''));
-    if (message.includes(data[0]) && data[2])
-      msg.reply(data[1] + (data[3] ? ` ${msg.author.username}` : ''));
+  include: (msg, data) => {
+    if (data[4] == 'reply')
+      msg.reply(`${data[1]} ${data[3] ? msg.author.username : ''}`);
+    if (data[4] == 'channel')
+      msg.channel.send(`${data[1]} ${data[3] ? msg.author.username : ''}`);
   },
 };
