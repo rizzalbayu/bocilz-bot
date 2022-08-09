6d4648b94fb3a45ebfa1fb39f2b7ee9d35643ed0
rizzalbayu <rizzalbayu21@gmail.com>
2022-08-09 11:01:09 +0000

    modify message created

--- events/message-create.js
+++ events/message-create.js
@@ -24,13 +24,21 @@ module.exports = {
       }
     } else {
       for (const message of dataMessage.messages) {
+        // array 0 = key(trigger), array 1 = response, array 2 = include, array 3 = with author, array 4 = reply or broadcast channel
         const userMessage = msg.content.toLowerCase();
         if (userMessage === message[0] && !message[2]) {
-          autoMessage.static(msg, message);
+          this.sent(msg, message);
         } else if (userMessage.includes(message[0]) && message[2]) {
-          autoMessage.include(msg, message);
+          this.sent(msg, message);
         }
       }
     }
+  },
+  sent: (msg, data) => {
+    if (data[4] == 'gif') msg.channel.send(data[1]);
+    if (data[4] == 'reply')
+      msg.reply(`${data[1]} ${data[3] ? msg.author.username : ''}`);
+    if (data[4] == 'channel')
+      msg.channel.send(`${data[1]} ${data[3] ? msg.author.username : ''}`);
   },
 };
